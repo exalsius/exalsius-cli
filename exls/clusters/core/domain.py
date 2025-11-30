86e47f3239d5e144f7b2c08f9c9c7712ffbf7bb0
srnbckr <soeren.becker@logsight.ai>
2025-11-30 17:15:09 +0100

    fix(cluster-create): convert cluster type string to upper-case

--- exls/clusters/core/domain.py
+++ exls/clusters/core/domain.py
@@ -17,7 +17,7 @@ class ClusterType(StrEnum):
     @classmethod
     def from_str(cls, value: str) -> ClusterType:
         try:
-            return cls(value)
+            return cls(value.upper())
         except ValueError:
             return cls.UNKNOWN
 
